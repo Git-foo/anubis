3f725433aa099f0ba42bd60ecb98fa982f1a580d
Gitfoo <neeko.marx@icloud.com>
2025-04-28 03:42:04 -0600

    Modified by www.SourceFiles.app

---
+++ xess/static/static/xess.go
@@ -0,0 +1,36 @@
+// Package xess vendors a copy of Xess and makes it available at /.xess/xess.css
+//
+// This is intended to be used as a vendored package in other projects.
+package xess
+
+import (
+	"embed"
+	"net/http"
+	"path/filepath"
+
+	"github.com/TecharoHQ/anubis"
+	"github.com/TecharoHQ/anubis/internal"
+)
+
+//go:generate go tool github.com/a-h/templ/cmd/templ generate
+
+var (
+	//go:embed *.css static
+	Static embed.FS
+
+	URL = "/.within.website/x/xess/xess.css"
+)
+
+func init() {
+	Mount(http.DefaultServeMux)
+
+	if anubis.Version != "devel" {
+		URL = filepath.Join(filepath.Dir(URL), "xess.min.css")
+	}
+
+	URL = URL + "?cachebuster=" + anubis.Version
+}
+
+func Mount(mux *http.ServeMux) {
+	mux.Handle("/.within.website/x/xess/", internal.UnchangingCache(http.StripPrefix("/.within.website/x/xess/", http.FileServerFS(Static))))
+}
